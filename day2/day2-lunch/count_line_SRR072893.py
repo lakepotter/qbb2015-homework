d236871ee7064aba5dc69991ce125d1ccaf38795
Lake Potter <lakepotter@mac.com>
2015-08-25 12:16:23 -0400

    line count for SRR072893.py

---
+++ day2/day2-lunch/count_line_SRR072893.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+filename = "/Users/cmdb/qbb2015-homework/day2/day2-lunch/SRR072893.sam"
+
+f = open (filename)
+
+num_lines=sum(1 for line in f)
+print num_lines
+
+
+
+
+
+
+    
