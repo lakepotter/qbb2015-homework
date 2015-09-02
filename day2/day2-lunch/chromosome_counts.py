0fd131fe4718e0b1df19266ca7b59d1cbffa57c2
Lake Potter <lakepotter@mac.com>
2015-09-02 12:20:43 -0400

    updated chromosome counts with small hack to get around broken file

--- day2/day2-lunch/chromosome_counts.py
+++ day2/day2-lunch/chromosome_counts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-filename = "/Users/cmdb/qbb2015-homework/day2/day2-lunch/SRR072893.sam"
+filename = "/Users/cmdb/qbb2015/genomes/SRR072893.sam"
 
 f = open (filename)
 
@@ -11,21 +11,25 @@ chromosome_counts = {"2L":0, "2R":0, "3L":0, "3R":0, "4":0, "X":0}
 
 
 for lines in f:
-    columns = lines.split()
-    chromosome = columns[2]
     if "@" in lines:
         #to avoid the header
         pass
-    elif chromosome in chromosome_counts:
-           chromosome_counts[chromosome] += 1
+    if "SRR072893.17886472" in lines:
+        break #I think I ended up with an erroneous .sam file - I kept getting a "list index out of range" error at the line where "chromosomes = columns[2]" because there was one line at the very end that just had SRR072893.1788 in the first column and nothing else. So I broke at the line before that (because that line had a * marked as its chromosome so it wouldn't be counted anyway) to get around the error. And I think it worked?
     else:
-        pass
+        columns = lines.split()
+        #print lines
+        chromosome = columns[2]
+        
+        if chromosome in chromosome_counts:
+           chromosome_counts[chromosome] += 1
+        else:
+            pass
 
 
 print chromosome_counts
 
 
-#I'm getting an error that says "list index out of range" but I can't figure out
 
 
 
