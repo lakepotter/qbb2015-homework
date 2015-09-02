c3b74299db8145b1b171c90721d04c5c2aa4cda3
Lake Potter <lakepotter@mac.com>
2015-09-02 12:49:05 -0400

    chromosome counts with complete .sam file - no errors

--- day2/day2-lunch/chromosome_counts.py
+++ day2/day2-lunch/chromosome_counts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-filename = "/Users/cmdb/qbb2015/genomes/SRR072893.sam"
+filename = "/Users/cmdb/qbb2015/genomes/SRR072893_2.sam"
 
 f = open (filename)
 
@@ -14,8 +14,7 @@ for lines in f:
     if "@" in lines:
         #to avoid the header
         pass
-    if "SRR072893.17886472" in lines:
-        break #I think I ended up with an erroneous .sam file - I kept getting a "list index out of range" error at the line where "chromosomes = columns[2]" because there was one line at the very end that just had SRR072893.1788 in the first column and nothing else. So I broke at the line before that (because that line had a * marked as its chromosome so it wouldn't be counted anyway) to get around the error. And I think it worked?
+   
     else:
         columns = lines.split()
         #print lines
